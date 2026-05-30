34c223c873a9654831d5c616436eb389310c19e9
zacdogwater <zacsengines@gmail.com>
2026-05-30 12:45:43 -0600

    bash is so tuff

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo pacman -Sy --needed --noconfirm - < $root/packages.txt
 mkdir -p ~/.config/nvim
 
 cat $root/configs.txt | while read -r dst src; do
-	ln -sfTv $root/config/$dst $src
+	ln -sfTv $root/config/$dst ~/$src
 done
 
 cat << EOF | sudo systemctl edit --stdin getty@tty1.service
@@ -19,6 +19,9 @@ EOF
 nvim -c :UpdatePlugins -c :q
 
 systemctl --user --now enable pipewire pipewire-pulse wireplumber
+
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
 
 fastfetch
 
